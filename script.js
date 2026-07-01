1957b9265f1e548d618b6b02b38c2ea0a094a0de
Daniel Steven Lopez Perez <dast11lp@gmail.com>
2026-07-01 18:38:26 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const prompt = require('prompt-sync')()
+
+
+
+let numero = prompt('digite un número: ');
+
+const factorial = (num) => {
+
+    if (num === 0) return 1;
+
+    let factorial = 1;
+    for (let i = 1; i <= num; i++) {
+        factorial = factorial * i
+        
+    }
+    return factorial
+}
+
+console.log(factorial(numero));
+
